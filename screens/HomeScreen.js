13513b8bf2c940d81b0b16d8a960c28c1f4fc203
Dan Trauten <dan.trauten@gmail.com>
2023-04-27 15:04:40 -0500

    Navigation Part 2

---
+++ screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const HomeScreen = () => {
+    reutrn(
+        <View>
+            <Text>Home Screen</Text>
+        </View>
+    );
+};
+
+export default HomeScreen;
