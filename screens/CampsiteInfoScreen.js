68af1785d5ef1b254c87d125eab867f7d6c8c675
Dan Trauten <dan.trauten@gmail.com>
2023-05-09 16:52:14 -0500

    Favorites

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -9,7 +9,7 @@ import { toggleFavorite } from '../features/favorites/favoritesSlice';
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
     const comments = useSelector((state) => state.comments);
-    const favorites = useSelector((state) => state.comments);
+    const favorites = useSelector((state) => state.favorites);
     const dispatch = useDispatch();
 
     const [showModal, setShowModal] = useState(false);
